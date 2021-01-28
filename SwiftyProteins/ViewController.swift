db561fe5fd16faea0de3a97530cbb61caf0f8f6d
Dmitrii <dp20121989@mail.ru>
2021-01-28 15:14:08 +0300

    Initial Commit

---
+++ SwiftyProteins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyProteins
+//
+//  Created by Дмитрий on 28.01.2021.
+//  Copyright © 2021 home. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
