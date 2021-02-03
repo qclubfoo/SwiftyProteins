10e262c614481e29924ed9c40f525f701dc9201a
Dmitrii <dp20121989@mail.ru>
2021-02-03 16:52:06 +0300

    ligand downloading first iteration complete

--- SwiftyProteins/AppDelegate.swift
+++ SwiftyProteins/AppDelegate.swift
@@ -15,9 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = TestViewController.storyboardInstance()
+//        window?.rootViewController = TestViewController.storyboardInstance()
+        window?.rootViewController = ProteinListVC.storyboardInstance()
         window?.makeKeyAndVisible()
-        showLoginVC()
+//        showLoginVC()
         return true
     }
 }
