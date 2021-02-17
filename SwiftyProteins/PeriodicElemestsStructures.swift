b7bcddb738fa820b37f6b24f8d9e81a50005b457
MixFon <mixfon@rambler.ru>
2021-02-17 19:00:40 +0300

    delete unuser fields in struct Elemtnts

--- SwiftyProteins/PeriodicElemestsStructures.swift
+++ SwiftyProteins/PeriodicElemestsStructures.swift
@@ -6,11 +6,6 @@
 //  Copyright © 2021 home. All rights reserved.
 //
 
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let periodicElemestsStructures = try? newJSONDecoder().decode(PeriodicElemestsStructures.self, from: jsonData)
-
 import Foundation
 
 // MARK: - PeriodicElemestsStructures
@@ -21,49 +16,19 @@ struct PeriodicElemestsStructures: Codable {
 // MARK: - Element
 struct Element: Codable {
     let name: String
-    let appearance: String?
     let atomicMass: Double?
     let boil: Double?
-    let category: String
-    let color: String?
     let density: Double?
-    let discoveredBy: String?
-    let melt, molarHeat: Double?
-    let namedBy: String?
-    let number, period: Int
-    let phase: Phase
-    let source: String
-    let spectralImg: String?
-    let summary, symbol: String
-    let xpos, ypos: Int
-    let shells: [Int]
-    let electronConfiguration, electronConfigurationSemantic: String
-    let electronAffinity, electronegativityPauling: Double?
-    let ionizationEnergies: [Double]
-    let cpkHex: String?
+    let melt: Double?
+    let symbol: String
+    let electronConfiguration: String
 
     enum CodingKeys: String, CodingKey {
-        case name, appearance
+        case name
         case atomicMass = "atomic_mass"
-        case boil, category, color, density
-        case discoveredBy = "discovered_by"
+        case boil, density
         case melt
-        case molarHeat = "molar_heat"
-        case namedBy = "named_by"
-        case number, period, phase, source
-        case spectralImg = "spectral_img"
-        case summary, symbol, xpos, ypos, shells
+        case symbol
         case electronConfiguration = "electron_configuration"
-        case electronConfigurationSemantic = "electron_configuration_semantic"
-        case electronAffinity = "electron_affinity"
-        case electronegativityPauling = "electronegativity_pauling"
-        case ionizationEnergies = "ionization_energies"
-        case cpkHex = "cpk-hex"
     }
-}
-
-enum Phase: String, Codable {
-    case gas = "Gas"
-    case liquid = "Liquid"
-    case solid = "Solid"
 }
